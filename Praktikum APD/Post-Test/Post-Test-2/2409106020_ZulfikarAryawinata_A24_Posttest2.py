5f02eb34e73c3d0b6252c24b2989316a040ba897
MamangolivE <zulfikararyawinata650@gmail.com>
2024-09-17 09:17:55 +0800

    Update 2409106020_ZulfikarAryawinata_A24_Posttest2.py

--- Praktikum APD/Post-Test/Post-Test-2/2409106020_ZulfikarAryawinata_A24_Posttest2.py
+++ Praktikum APD/Post-Test/Post-Test-2/2409106020_ZulfikarAryawinata_A24_Posttest2.py
@@ -4,14 +4,14 @@ Panggilan = input("Nama Panggilan :")
 Prodi = input("Masukkan Prodi Anda :")
 Fakultas = input("Masukkan Fakultas Anda :")
 Universitas = input("Masukkan Universitas Anda :")
-usia:float = (input("Berapa Usia Anda :"))
+usia:int = (input("Berapa Usia Anda :"))
 Tinggi:float = (input("Berapa Tinggi Badan Anda :"))
 BB:float = (input("Berapa Berat Badan Anda :"))
 Hobi = input("Apa Hobi Anda :")
 
-print("Halo, perkenalkan nama saya ", Nama, "biasa dipanggil ", Panggilan,". NIM saya adalah", NIM,
+print("Halo, perkenalkan nama saya", Nama, ", biasa dipanggil", Panggilan,". NIM saya adalah", NIM,
       ", saya dari Program Studi", Prodi,", Fakultas", Fakultas,"," ,Universitas,". Sekarang saya berusia", usia,"tahun"
-      ". Tinggi badan saya adalah", Tinggi,"cm"" dan berat badan saya adalah", BB,"Kg"". Saya memiliki hobi", Hobi,".")
+      ". Tinggi badan saya adalah", Tinggi,"cm dan berat badan saya adalah", BB,"Kg. Saya memiliki hobi", Hobi,".")
 if int(NIM[7:10])>=100:
     print("3 digit terakhir NIM =", NIM[7:10], "% 6")
 else:
